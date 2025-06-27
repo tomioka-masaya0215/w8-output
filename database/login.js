a02b5b6ed533eac5b71fc00f2280b0e17b951544
冨岡 雅也 <tomioka.masaya0215@gmail.com>
2025-06-28 01:02:15 +0900

    2025/06/28 STEP8の課題

---
+++ database/login.js
@@ -0,0 +1,28 @@
+//初期表示時の処理
+//ローカルストレージからログイン情報を取得する
+let getItemEmail = localStorage.getItem('email');
+let getItemPass = localStorage.getItem('password');
+if(getItemEmail != "" && getItemPass != "") {
+    // ログイン情報を入力欄にセットする
+    document.getElementById('email').value = getItemEmail;
+    document.getElementById('password').value = getItemPass;
+} else {
+    //なにもしない
+}
+
+//Signin押下時の処理
+function setLocalStorage(){
+    //ローカルストレージからログイン情報を取得する
+    let getItemEmail = localStorage.getItem('email');
+    let getItemPass = localStorage.getItem('password');
+    if(getItemEmail == "" && getItemPass == "") {
+        //フォームから入力されたログイン情報を取得する
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('password').value;
+        //ログイン情報をLocalStorageに保存する
+        localStorage.setItem('email', email);
+        localStorage.setItem('password', password);
+    } else {
+        //なにもしない
+    }
+}
